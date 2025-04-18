efd6ff784cc1be606851b5f7f8ed57c87a9305c4
crop <crop_tech@proton.me>
2025-04-18 11:51:08 +0200

    draft empty enum with discriminants

--- strum_macros/src/macros/enum_discriminants.rs
+++ strum_macros/src/macros/enum_discriminants.rs
@@ -136,7 +136,11 @@ pub fn enum_discriminants_inner(ast: &DeriveInput) -> syn::Result<TokenStream> {
         })
         .collect::<Vec<_>>();
 
-    let from_fn_body = quote! { match val { #(#arms),* } };
+    let from_fn_body = if variants.is_empty() {
+        quote! { match val {  _ => todo!(), } }
+    } else {
+        quote! { match val { #(#arms),* } }
+    };
 
     let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
     let impl_from = quote! {
